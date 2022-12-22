ce130408832601e9992aedbe3318799d593f40ac
Braqzen <sydney.boroch@fuel.sh>
2022-12-22 23:14:08 +0000

    Cleaned up first version

---
+++ README.md
@@ -0,0 +1,7 @@
+
+> TODO: Lazy documentation
+
+1. Go to [coinmarketcap](https://coinmarketcap.com/api/) and create an API key.
+2. Copy the file `config.json.ex` and name it `config.json`
+3. In `config.json` enter your API key
+4. Run the executable with `cargo run`
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,38 @@
+mod api;
+mod config;
 
-extern crate serde;
-extern crate serde_json;
+use api::Response;
+use config::Config;
 
-//fn main (){                              //mess as I thought i would come back to it later and try to get api key
-                                            // to load .
+fn main() {
+    // Add error handling for I/O errors
+    let text = std::fs::read_to_string("config.json").unwrap();
+    let config = serde_json::from_str::<Config>(&text).unwrap();
 
-// #[derive(Deserialize)]
-// #[derive(Debug)]
-//#[derive(Clone)]
-//#[derive(Eq, Hash, PartialEq)]
+    let cm_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest".to_string();
+    let client = reqwest::blocking::Client::new();
 
-// struct Apidata{
-//     apikey: String
-// }
+    // TODO: make query parameters configurable
+    let response = client
+        .get(cm_url)
+        .header("X-CMC_PRO_API_KEY", config.apikey)
+        .query(&[("start", "1"), ("limit", "5"), ("convert", "GBP")])
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
 
+    let result: Response = serde_json::from_str(&response).unwrap();
 
-// let apikey = std::fs::read_to_string("apidata.json").unwrap();
-// let accepts = "Accepts:application/json" ;
-fn main (){
-
-let cm_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest".to_string();
-let client = reqwest::blocking::Client::new();
-
-
-let res = client.get(cm_url)
-.header("X-CMC_PRO_API_KEY","need API key hardcoded here")// Hardcoded here as 
-// I couldn't get the api key to load just its value from file, was always the key value pair. 
-.query(&[("start", "1"),("limit","20"),("convert","GBP")])//parameters
-.send()
-.unwrap()
-.text()
-.unwrap();
-
-let json: serde_json::Value = serde_json::from_str(&res).unwrap();
-let items = json.get("data").unwrap();//have tried index :"quote" here but does not work
-let cryptos = items.as_array().unwrap();
-
- for crypto in cryptos{
-    let name=crypto["name"].as_str().unwrap();
-    let symbol = crypto["symbol"].as_str().unwrap();
-    let price = crypto["quote"].as_str();// fails here as None value in quote if .unwrap(); is added .    
-    //let last = format!("${:.20}", crypto["quote"].as_f64().unwrap()); // was something i tried and failed.
-    println!("{} {} {:?}",name,symbol,price);
-
-}    
+    // TODO: can check status to see if error, inform user of error or print result if ok
+    for crypto in result.data {
+        // TODO: make currency configurable
+        println!(
+            "Name: {}\nSymbol: {}\nPrice £{}\n24h % Change: {}\n",
+            crypto.name,
+            crypto.symbol,
+            crypto.quote.get("GBP").unwrap().price,
+            crypto.quote.get("GBP").unwrap().percent_change_24h
+        );
+    }
 }
-
-// TODO : I cannot get the array to convert to a list i can index into .
-// "quote" seems to be a nested array/dict , need to access this, key "quote" is not doing it.
