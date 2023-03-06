9f5a220eb1b8bd4e44e4d6b0de2c4eb9bba3d7d8
GeckoSplat <111183176+GeckoSplat@users.noreply.github.com>
2023-03-06 21:32:26 +0000

    Added CLAP crate and args input

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,48 @@
 mod api;
 mod config;
 
-use api::Response;
+use clap::Parser;
+//std::env::args;
+use anyhow::Result;
+use api::{Response, Status};
 use config::Config;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    start: String,
+
+    #[arg(short, long)]
+    limit: String,
+
+    #[arg(short, long)]
+    convert: String,
+}
 
 fn main() {
     // Add error handling for I/O errors
+    // cannot figure out try catch in rust
+
+    let args = Args::parse();
+
+    println!("{},{},{}", args.start, args.limit, args.convert);
+
     let text = std::fs::read_to_string("config.json").unwrap();
     let config = serde_json::from_str::<Config>(&text).unwrap();
 
     let cm_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest".to_string();
     let client = reqwest::blocking::Client::new();
 
-    // TODO: make query parameters configurable
     let response = client
         .get(cm_url)
         .header("X-CMC_PRO_API_KEY", config.apikey)
-        .query(&[("start", "1"), ("limit", "5"), ("convert", "GBP")])
+        .query(&[
+            ("start", args.start),
+            ("limit", args.limit),
+            ("convert", args.convert),
+        ])
         .send()
         .unwrap()
         .text()
@@ -25,8 +51,10 @@ fn main() {
     let result: Response = serde_json::from_str(&response).unwrap();
 
     // TODO: can check status to see if error, inform user of error or print result if ok
+
     for crypto in result.data {
         // TODO: make currency configurable
+        // need to change url and api.rs to manage this
         println!(
             "Name: {}\nSymbol: {}\nPrice £{}\n24h % Change: {}\n",
             crypto.name,
